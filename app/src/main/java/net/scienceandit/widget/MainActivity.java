b6b9e3f6005cdda3e5fb01a4b5eab723b605f3ad
Łukasz Fidurski <fidurski.l@gmail.com>
2015-06-08 19:27:12 +0200

    widget

---
+++ app/src/main/java/net/scienceandit/widget/MainActivity.java
@@ -0,0 +1,30 @@
+package net.scienceandit.widget;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private Button btnUpdateWidget;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        btnUpdateWidget = (Button) findViewById(R.id.btnUpdateWidget);
+        btnUpdateWidget.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                updateWidget();
+            }
+        });
+    }
+
+    private void updateWidget() {
+        Intent intent = new Intent();
+        intent.setAction(MyAppWidget.UPDATE_ON_DEMAND);
+        sendBroadcast(intent);
+    }
+}
